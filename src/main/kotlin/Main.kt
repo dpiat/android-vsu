a4e06434a9afc4bffd246f6d4417b5301f791949
dpiataikin <dpiataikin@gmail.com>
2023-10-25 17:03:55 +0300

    add comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-import java.net.URL
-
 fun main() {
+    // read file from resources
     var names: List<String> = FileWorker.readFile("names.txt")
     names = names.sorted()
+    // write file to build/resources/main
     FileWorker.writeFile("names_out.txt", names)
 }
 
